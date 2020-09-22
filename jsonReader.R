4612cd03620862e0b34d7bfaaec9e89f70337451
Tadeu Gomes <32282635+tadeugomes@users.noreply.github.com>
2020-09-22 11:51:29 -0300

    Update jsonReader.R

--- jsonReader.R
+++ jsonReader.R
@@ -16,7 +16,7 @@ library(utf8)
 #---------------------------------------------------------
 #Download JSON File from Instagram for a specific Hashtag
 #---------------------------------------------------------
-hashtag <- "psicologiaclinica"
+hashtag <- "hashtag"
 url_start <- str_glue("http://instagram.com/explore/tags/{hashtag}/?__a=1")
 json <- fromJSON(url_start)
 edge_hashtag_to_media <- json$graphql$hashtag$edge_hashtag_to_media
